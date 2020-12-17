3304d69e9b75ef03fcb72da791bd0b06bad4f254
Joachim Schuster <mail@joachimschuster.de>
2020-12-17 19:41:59 +0100

    Implement merge of odd and even pages with qpdf. Improve temp file handling of interims files. Make sure to process only fully loaded files (heavily inspired by ocrmypdf).

--- watcher.py
+++ watcher.py
@@ -21,8 +21,10 @@
 import sys
 import shutil
 import os
+import tempfile
 import time
 import logging
+import pikepdf
 
 from re import search
 from pathlib import Path
@@ -32,6 +34,7 @@ from watchdog.events import PatternMatchingEventHandler
 
 INPUT_DIRECTORY = os.getenv('INPUT_DIRECTORY', '/input')
 OUTPUT_DIRECTORY = os.getenv('OUTPUT_DIRECTORY', '/output')
+POLL_NEW_FILE_SECONDS = int(os.getenv('POLL_NEW_FILE_SECONDS', '1'))
 PDF_PATTERNS = ['*.pdf', '*.PDF']
 ODD_PAGES_PATTERN = 'front'
 EVEN_PAGES_PATTERN = 'back'
@@ -43,22 +46,25 @@ log = logging.getLogger('pdf-merge-watcher')
 
 def prepare_pages(file_path):
     file_path = Path(file_path)
+    if not wait_for_file_ready(file_path):
+        log.info(f"Gave up waiting for {file_path} to become ready")
+        return
     if search(ODD_PAGES_PATTERN, file_path.name):
-        shutil.move(file_path, os.path.join(OUTPUT_DIRECTORY, "odd.pdf"))
+        shutil.move(file_path, os.path.join(tempfile.gettempdir(), "odd.pdf"))
     elif search(EVEN_PAGES_PATTERN, file_path.name):
-        shutil.move(file_path, os.path.join(OUTPUT_DIRECTORY, "even.pdf"))
+        shutil.move(file_path, os.path.join(tempfile.gettempdir(), "even.pdf"))
     else:
         log.info('PDF does not match even nor odd pages pattern')
 
 
 def execute_pdf_merge():
-    odd_file = Path(os.path.join(OUTPUT_DIRECTORY, "odd.pdf"))
-    even_file = Path(os.path.join(OUTPUT_DIRECTORY, "even.pdf"))
+    odd_file = Path(os.path.join(tempfile.gettempdir(), "odd.pdf"))
+    even_file = Path(os.path.join(tempfile.gettempdir(), "even.pdf"))
     if odd_file.exists() & even_file.exists():
         log.info('Begin PDF merging')
         timestr = time.strftime("%Y%m%d-%H%M%S")
         merged_file = Path(os.path.join(OUTPUT_DIRECTORY, MERGED_NAME_PREFIX + "_" + timestr + ".pdf"))
-        call(["touch", merged_file])
+        call(["qpdf", "--collate", odd_file, "--pages", odd_file, even_file, "z-1", "--", merged_file])
         log.info('Successful merged, removing source files')
         odd_file.unlink()
         even_file.unlink()
@@ -68,6 +74,29 @@ def execute_pdf_merge():
         log.info('Waiting for odd pages')
     else:
         log.info('No files to merge found')
+
+
+def wait_for_file_ready(file_path):
+    # This loop waits to make sure that the file is completely loaded on
+    # disk before attempting to read. Docker sometimes will publish the
+    # watchdog event before the file is actually fully on disk, causing
+    # pikepdf to fail.
+
+    retries = 5
+    while retries:
+        try:
+            pdf = pikepdf.open(file_path)
+        except (FileNotFoundError, pikepdf.PdfError) as e:
+            log.info(f"File {file_path} is not ready yet")
+            log.debug("Exception was", exc_info=e)
+            time.sleep(POLL_NEW_FILE_SECONDS)
+            retries -= 1
+        else:
+            pdf.close()
+            return True
+
+    return False
+
 
 class EventHandler(PatternMatchingEventHandler):
     def on_any_event(self, event):
